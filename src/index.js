ce9e99d82f1a8ad59e3ea3433b71b94aa8c4fe77
Vishnu V V <ng.pingme@gmail.com>
2018-12-07 01:25:22 +0530

    Add a wrapper for localization

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Index from './pages/index';
+import { IntlProvider, addLocaleData } from "react-intl";
 import * as serviceWorker from './serviceWorker';
+import translations from "./i18n/locales"
 
-ReactDOM.render(<Index />, document.getElementById('root'));
+import esLocaleData from "react-intl/locale-data/es";
+
+addLocaleData(esLocaleData);
+
+// get locale from url
+const locale = window.location.search.replace("?locale=","") || "en"
+const messages = translations[locale];
+
+ReactDOM.render(
+  <IntlProvider locale={locale} key={locale} messages={messages}>
+    <Index />
+  </IntlProvider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
